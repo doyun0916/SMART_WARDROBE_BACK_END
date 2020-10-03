1f0e4ff906aec0f7f5192f95f9075e0c370cf53f
Ubuntu <ubuntu@ip-172-31-18-252.us-east-2.compute.internal>
2020-10-03 14:55:06 +0000

    clothes app added

--- wardrobe/urls.py
+++ wardrobe/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include('restApi.urls')),
+    url(r'^', include('clothes.urls')),
 ]
