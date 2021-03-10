a98f70f44d3d97c601300685375d1dafe7e50fa3
doyun0916 <52279390+doyun0916@users.noreply.github.com>
2021-03-11 01:13:14 +0900

    Update clothes_classification_model(transfer learning, resnet).py

--- clothes_classification_proto/Pytorch_rough/clothes_classification_model(transfer learning, resnet).py
+++ clothes_classification_proto/Pytorch_rough/clothes_classification_model(transfer learning, resnet).py
@@ -15,10 +15,7 @@ import copy
 
 
 def main():
-    plt.ion()  # interactive mode
-
-    # Data augmentation and normalization for training
-    # Just normalization for validation
+    
     data_transforms = {
         'train': transforms.Compose([
             transforms.RandomResizedCrop(224),
@@ -49,45 +46,39 @@ def main():
     inputs, classes = next(iter(dataloaders['train']))
 
     def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-        since = time.time()
+        
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
 
         best_model_wts = copy.deepcopy(model.state_dict())
         best_acc = 0.0
 
         for epoch in range(num_epochs):
-            print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-            print('-' * 10)
-
             for phase in ['train', 'val']:
                 if phase == 'train':
-                    model.train()  # Set model to training mode
+                    model.train()
                 else:
-                    model.eval()  # Set model to evaluate mode
+                    model.eval()
 
                 running_loss = 0.0
                 running_corrects = 0
 
-                # Iterate over data.
+                # Iterate
                 for inputs, labels in dataloaders[phase]:
                     inputs = inputs.to(device)
                     labels = labels.to(device)
 
-                    # zero the parameter gradients
                     optimizer.zero_grad()
 
-                    # forward
-                    # track history if only in train
-                    with torch.set_grad_enabled(phase == 'train'):
+                    with torch.set_grad_enabled(phase == 'train'): # gradient activating
                         outputs = model(inputs)
                         _, preds = torch.max(outputs, 1)
                         loss = criterion(outputs, labels)
-
-                        # backward + optimize only if in training phase
+                        
                         if phase == 'train':
                             loss.backward()
                             optimizer.step()
-
-                    # statistics
+                    
                     running_loss += loss.item() * inputs.size(0)
                     running_corrects += torch.sum(preds == labels.data)
                 if phase == 'train':
@@ -104,11 +95,6 @@ def main():
                     best_acc = epoch_acc
                     best_model_wts = copy.deepcopy(model.state_dict())
 
-            print()
-
-        time_elapsed = time.time() - since
-        print('Training complete in {:.0f}m {:.0f}s'.format(
-            time_elapsed // 60, time_elapsed % 60))
         print('Best val Acc: {:4f}'.format(best_acc))
 
         # load best model weights
@@ -124,22 +110,15 @@ def main():
 
     criterion = nn.CrossEntropyLoss()
 
-    # Observe that all parameters are being optimized
     optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 
-    # Decay LR by a factor of 0.1 every 7 epochs
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
     model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
                            num_epochs=20)
 
     torch.save(model_ft, "top_short.pt")
-    visualize_model(model_ft)
-
-    plt.ioff()
-    plt.show()
-
 
 if __name__ == '__main__':
     torch.multiprocessing.freeze_support()
-    main()  # execute this only when run directly, not when imported!
+    main()
